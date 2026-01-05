545fabedf88cfd0bb4bc601f21ae8a2f47b41d33
donatme <xapqax@gmail.com>
2026-01-05 18:11:29 +0100

    MyArrayList 1.1

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,51 +1,82 @@
 
-public class MyArrayList<E> {
 
-    int size = 0;
-    Object[] arrays = new Object[10];
+public class MyArrayList<E> implements List<E> {
+    private int size = 0;
+    private Object[] items = new Object[10];
 
-    static void main() {
-        MyArrayList<String> list = new MyArrayList<>();
-        list.add("a");
-        list.add("b");
-        list.add("C");
-        for (int i = 0; i < list.size; i++) {
-            System.out.println(list.get(i));
-        }
+    @Override
+    public int size() {
+        return size;
     }
 
-    void add(E o) {
-        if (size < arrays.length) {
-            arrays[size] = o;
+    @Override
+    public void add(E element) {
+        if (size < items.length) {
+            items[size] = element;
             size++;
         } else {
-            Object[] temp = new Object[arrays.length + arrays.length / 2];
+            Object[] temp = new Object[items.length + items.length / 2];
             for (int i = 0; i < size; i++) {
-                temp[i] = arrays[i];
+                temp[i] = items[i];
             }
-            arrays = temp;
-            arrays[size] = o;
+            items = temp;
+            items[size] = element;
             size++;
         }
     }
 
-    E get(int i) {
-        if (i >= 0 && i < size) {
-            return (E) arrays[i];
-        } else throw new IndexOutOfBoundsException();
+    @Override
+    public void addByIndex(E element, int index) {
+
+        if (element == null) {
+            throw new NullPointerException();
+        } else if (index < 0) {
+            throw new IllegalArgumentException();
+        } else if (index > size() - 1) {
+            throw new MyIndexOutBoundsException();
+        }
+        if (size == items.length) {
+            Object[] temp = new Object[items.length + items.length / 2];
+            for (int i = 0; i < size; i++) {
+                temp[i] = items[i];
+            }
+            items = temp;
+        }
+        for (int i = size; i > index; i--) {
+            items[i] = items[i - 1];
+        }
+        items[index] = element;
+        size++;
+
     }
 
-    E remove(int i) {
+    @Override
+    public E getByIndex(int i) {
+        if (i >= 0 && i < size) {
+            return (E) items[i];
+        } else throw new MyIndexOutBoundsException();
+    }
+
+    @Override
+    public E removeByIndex(int i) {
         if (i > size - 1 || i < 0) {
-            throw new IndexOutOfBoundsException();
+            throw new MyIndexOutBoundsException();
         } else {
-            Object removed = arrays[i];
+            Object removed = items[i];
             for (int j = i; j < size - 1; j++) {
-                arrays[j] = arrays[j + 1];
+                items[j] = items[j + 1];
             }
             size--;
-            arrays[size] = null;
+            items[size] = null;
             return (E) removed;
         }
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            items[i] = null;
+        }
+        size = 0;
     }
 }
